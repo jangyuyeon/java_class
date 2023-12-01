f053aa7effdf93facc6c9fef1a93ad79cbf06695
pork74 <pork74@hanmail.net>
2023-12-01 16:26:00 +0900

    test

---
+++ src/ch04_if/Ex04_IfExample.java
@@ -0,0 +1,34 @@
+package ch04_if;
+import java.util.Scanner;
+
+
+public class Ex04_IfExample {
+
+	public static void main(String[] args) {
+		/*
+		 * 스캐너로 점수(score)를 하나 입력받아서
+		 * 90점 이상이면 A
+		 * 80점 이상이면 B
+		 * 70점 이상이면 C
+		 * 60점 이상이면 D
+		 * 60점보다 적은 점수면 F
+		 * 를 출력
+		 */
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("점수: ");
+		int num1 = scanner.nextInt();
+		
+		if(num1 >= 90) {
+			System.out.println("A");
+		}else if(num1 >= 80) {
+			System.out.println("B");
+		}else if(num1 >= 70) {
+			System.out.println("C");
+		}else if(num1 >= 60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+	}
+	
+}
---
+++ src/ch04_if/Ex05_IfExample.java
@@ -0,0 +1,37 @@
+package ch04_if;
+import java.util.Scanner;
+
+public class Ex05_IfExample {
+
+	public static void main(String[] args) {
+		/*
+		 * 중첩 if문을 적용한 학점 출력
+		 * 100점을 초과하거나 0점 미만의 점수를 입력하면 '입력범위를 초과하였습니다.'출력
+		 * 0~100 점 사이의 값을 입력했다면 앞의 예제에서 했던 학점 출력을 진행함.
+		 * 단, 각 조건마다 학점값을 출력하는 것이 아니라 학점 출력을 위한 print 문장은 한번만 사용
+		 */
+		Scanner scanner = new Scanner(System.in);
+				System.out.println("점수: ");
+		int num1 = scanner.nextInt();
+		String num2 = "";
+		
+		if(num1 > 100 || num1 < 0) {
+			System.out.println("입력범위를 초과하였습니다.");
+		}else{
+			if(num1 >= 90) {
+				num2 = "A";
+			}else if(num1 >= 80) {
+				num2 = "B";
+			}else if(num1 >= 70) {
+				num2 = "C";
+			}else if(num1 >= 60) {
+				num2 = "D";
+			}else {
+				num2 = "F";
+			}
+		    }
+		System.out.println("학점은" + num2 + "입니다.");
+		}	
+		}
+	
+
