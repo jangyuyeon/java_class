5804cfa4a909b3e6726fafc874a019b48903de9c
pork74 <pork74@hanmail.net>
2023-12-06 00:54:14 +0900

    자습

--- src/ch06_For/Ex21_ForEx.java
+++ src/ch06_For/Ex21_ForEx.java
@@ -10,7 +10,16 @@ public class Ex21_ForEx {
          * 2의 배수, 3의배수를 제외한 수의 총 합 출력
          */
         Scanner scanner = new Scanner(System.in);
+        int num = 0, sum = 0;
         System.out.println("숫자를 입력하세요: ");
-        int i = scanner.nextInt();
+        num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++){
+            if ((i % 2 != 0) || (i % 3 != 0)) {
+                System.out.println("i = " +i);
+                sum += i;
+            }
+        }
+                System.out.println(sum);
     }
 }
