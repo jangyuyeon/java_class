648f12ce90298fe59967fcdb8e6be56ec864b7f3
pork74 <pork74@hanmail.net>
2023-12-05 21:10:08 +0900

    자습

---
+++ src/ch06_For/Ex21_ForEx.java
@@ -0,0 +1,16 @@
+package ch06_For;
+
+import java.util.Scanner;
+
+public class Ex21_ForEx {
+    public static void main(String[] args) {
+        /**
+         * 정수를 하나 입력받고
+         * 1부터 입력받은 정수까지의 정수 중에서
+         * 2의 배수, 3의배수를 제외한 수의 총 합 출력
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요: ");
+        int i = scanner.nextInt()
+    }
+}
