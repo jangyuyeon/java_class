252087fa254bcec7f0b126b4d3f7261d9eec0277
pork74 <pork74@hanmail.net>
2023-12-08 16:44:32 +0900

    string연습

---
+++ src/ch08_string/Ex04_StringEx.java
@@ -0,0 +1,27 @@
+package ch08_string;
+
+import java.util.Scanner;
+
+public class Ex04_StringEx {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("메시지를 입력하세요");
+        System.out.println("프로그램을 종료하려면 '종료'를 입력하세요.");
+
+        String world = "";
+        boolean run = true;
+
+        while (run) {
+            System.out.print(">");
+            world = scanner.next();
+            System.out.print(world);
+
+            if (world.equals("종료")) {
+                run = false;
+
+            }
+        }
+
+    }
+}
